69acec810c78e2f277ac2079988f06828e9a70c4
Blazsoul <tamirdalal10@gmail.com>
2019-04-03 01:05:14 +0300

    Updates

---
+++ precache-manifest.a36b623abed035464f327bc3df504d7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "120880d557e2621775e4",
+    "url": "/Leadspage/static/js/runtime~main.2e4340b9.js"
+  },
+  {
+    "revision": "eba4cb430865b9e8a91b",
+    "url": "/Leadspage/static/js/main.2b701c2e.chunk.js"
+  },
+  {
+    "revision": "ea090ca4099509efd99c",
+    "url": "/Leadspage/static/js/2.8cc3a42e.chunk.js"
+  },
+  {
+    "revision": "eba4cb430865b9e8a91b",
+    "url": "/Leadspage/static/css/main.b7eb1c18.chunk.css"
+  },
+  {
+    "revision": "439e80db8f36385e191d5c2968113a7c",
+    "url": "/Leadspage/index.html"
+  }
+];
