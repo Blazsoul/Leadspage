b38e92d87269fe3584c1c2ab5be2f8b45f68a19f
Blazsoul <tamirdalal10@gmail.com>
2019-04-03 01:09:31 +0300

    Added gh pages, removed docs

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.65804af052a71ccca99e8fb8aed8c150.js"
+  "/Leadspage/precache-manifest.a36b623abed035464f327bc3df504d7d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Leadspage/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
